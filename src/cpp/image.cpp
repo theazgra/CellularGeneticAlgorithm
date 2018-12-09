eb57b44c21848685af423d61ae4d3c1fcfd5719d
theazgra <theazgra@gmail.com>
2018-12-09 20:49:28 +0100

    Basic interface for CImg Library.

---
+++ src/cpp/image.cpp
@@ -0,0 +1,60 @@
+#define cimg_display 0
+#include "CImg.h"
+
+typedef unsigned char uchar;
+
+struct RgbPixel
+{
+    uchar R;
+    uchar G;
+    uchar B;
+
+    RgbPixel()
+    {
+        R = 0;
+        G = 0;
+        B = 0;
+    }
+
+    RgbPixel(uchar r, uchar g, uchar b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
+    RgbPixel(uchar value)
+    {
+        R = value;
+        G = value;
+        B = value;
+    }
+};
+
+inline void set_pixel(cimg_library::CImg<uchar> &img, const uint &row, const uint &col, const RgbPixel &rgb)
+{
+    img(col, row, 0) = rgb.R;
+    img(col, row, 1) = rgb.G;
+    img(col, row, 2) = rgb.B;
+}
+
+inline RgbPixel get_pixel(cimg_library::CImg<uchar> &img, const uint &row, const uint &col)
+{
+    RgbPixel rgb;
+    rgb.R = img(col, row, 0);
+    rgb.G = img(col, row, 1);
+    rgb.B = img(col, row, 2);
+    return rgb;
+}
+
+cimg_library::CImg<uchar> create_grayscale_image(const uint width, const uint height)
+{
+    cimg_library::CImg<uchar> image(width, height, 1, 1);
+    return image;
+}
+
+cimg_library::CImg<uchar> create_color_image(const uint width, const uint height)
+{
+    cimg_library::CImg<uchar> image(width, height, 1, 3);
+    return image;
+}
---
+++ src/cpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "image.cpp"
+
+int main(int, char **)
+{
+    using namespace cimg_library;
+
+    CImg<unsigned char> img = create_color_image(100, 500);
+    img.fill(0);
+    for (uint row = 0; row < img.height(); row++)
+    {
+        for (uint col = 0; col < img.width(); col++)
+        {
+            set_pixel(img, row, col, RgbPixel(125, 80, 186));
+        }
+    }
+
+    img.save_png("Result.png");
+    return 0;
+}
